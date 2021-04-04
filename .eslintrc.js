4e87e6ab8cb529619ef752edf3c0548fb5326965
ramses <ramses.deco@gmail.com>
2021-04-04 14:48:32 -0500

    chore: add eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,30 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier',
+    '@react-native-community',
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': [
+      0,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react/jsx-filename-extension': [
+      0,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+  },
+  plugins: ['prettier'],
 };
