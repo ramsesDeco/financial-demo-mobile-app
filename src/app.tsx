3990881b5053afa2d13d1cac67925bbcfa911ae2
ramses <ramses.deco@gmail.com>
2021-04-04 18:40:50 -0500

    feat: add theme definition

--- src/app.tsx
+++ src/app.tsx
@@ -4,12 +4,14 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Provider as PaperProvider} from 'react-native-paper';
 
 import {RootNavigator} from './router/root-navigator';
+import {getTheme} from './theme';
 
 export const App = () => {
+  const theme = getTheme();
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <StatusBar />
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'} />
         <RootNavigator />
       </NavigationContainer>
     </PaperProvider>
---
+++ src/theme/theme.ts
@@ -0,0 +1,59 @@
+import {Appearance} from 'react-native';
+
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  configureFonts,
+} from 'react-native-paper';
+
+const CombinedDefaultTheme = {
+  ...PaperDefaultTheme,
+  ...NavigationDefaultTheme,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    ...NavigationDefaultTheme.colors,
+  },
+};
+
+const CombinedDarkTheme = {
+  ...PaperDarkTheme,
+  ...NavigationDarkTheme,
+  colors: {
+    ...PaperDarkTheme.colors,
+    ...NavigationDarkTheme.colors,
+  },
+};
+
+export const getTheme = () => {
+  const isDark = Appearance.getColorScheme() === 'dark';
+  const currentTheme = isDark ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  return {
+    ...currentTheme,
+    fonts: configureFonts({
+      default: {
+        regular: {
+          fontFamily: 'sans-serif',
+          fontWeight: 'normal',
+        },
+        medium: {
+          fontFamily: 'sans-serif-medium',
+          fontWeight: 'normal',
+        },
+        light: {
+          fontFamily: 'sans-serif-light',
+          fontWeight: 'normal',
+        },
+        thin: {
+          fontFamily: 'sans-serif-thin',
+          fontWeight: 'normal',
+        },
+      },
+    }),
+  };
+};
