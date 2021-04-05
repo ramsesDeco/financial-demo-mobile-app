1c7426da62fc3e79dfdd146bcfb4239a48fd4b8d
ramses <ramses.deco@gmail.com>
2021-04-04 19:31:42 -0500

    feat: add shortcut-features

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {View} from 'react-native';
 import {UserBalanceCard} from './user-balance-card';
 import {TransactionList} from './transaction-list';
+import {ShortcutFeatures} from './shortcut-features';
 
 export const Dashboard = () => {
   return (
     <View>
       <UserBalanceCard />
+      <ShortcutFeatures />
       <TransactionList />
     </View>
   );
