72ea535bebdbef60de82df6dfd67a88e805763f4
ramses <ramses.deco@gmail.com>
2021-04-04 22:56:26 -0500

    feat: add custom color to tab-navigator

--- src/router/tab-navigator.tsx
+++ src/router/tab-navigator.tsx
@@ -1,61 +1,69 @@
 import React from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {IconButton} from 'react-native-paper';
+import {IconButton, useTheme} from 'react-native-paper';
 import {Dashboard, NotWorkedView} from '../components';
 
 const Tab = createMaterialBottomTabNavigator();
 
-export const TabNavigator = () => (
-  <Tab.Navigator initialRouteName="Home">
-    <Tab.Screen
-      name="Home"
-      component={Dashboard}
-      options={{
-        tabBarLabel: '',
-        tabBarIcon: ({color}) => (
-          <IconButton icon="home-outline" color={color} size={26} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="Settings"
-      component={NotWorkedView}
-      options={{
-        tabBarLabel: '',
-        tabBarIcon: ({color}) => (
-          <IconButton icon="cash" color={color} size={26} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="CreditCardSettings"
-      component={NotWorkedView}
-      options={{
-        tabBarLabel: '',
-        tabBarIcon: ({color}) => (
-          <IconButton icon="credit-card-settings" color={color} size={26} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="Favorites"
-      component={NotWorkedView}
-      options={{
-        tabBarLabel: '',
-        tabBarIcon: ({color}) => (
-          <IconButton icon="star-outline" color={color} size={26} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="Notifications"
-      component={NotWorkedView}
-      options={{
-        tabBarLabel: '',
-        tabBarIcon: ({color}) => (
-          <IconButton icon="bell-outline" color={color} size={26} />
-        ),
-      }}
-    />
-  </Tab.Navigator>
-);
+export const TabNavigator = () => {
+  const {
+    colors: {surface},
+  } = useTheme();
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      barStyle={{backgroundColor: surface}}>
+      <Tab.Screen
+        name="Home"
+        component={Dashboard}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color}) => (
+            <IconButton icon="home-outline" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={NotWorkedView}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color}) => (
+            <IconButton icon="cash" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="CreditCardSettings"
+        component={NotWorkedView}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color}) => (
+            <IconButton icon="credit-card-settings" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={NotWorkedView}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color}) => (
+            <IconButton icon="star-outline" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotWorkedView}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({color}) => (
+            <IconButton icon="bell-outline" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
